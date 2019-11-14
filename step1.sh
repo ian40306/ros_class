f94d60959f3cc21c5af30b5019efb5a3a6875102
ian40306 <43855922+ian40306@users.noreply.github.com>
2019-11-14 09:51:57 +0000

    Add files via upload

---
+++ step1.sh
@@ -0,0 +1,7 @@
+sudo useradd -m $1
+sudo adduser $1 sudo
+sudo adduser $1 video
+sudo adduser $1 i2c
+sudo chsh -s /bin/bash $1
+sudo passed $1
+exit
---
+++ step2.sh
@@ -0,0 +1,19 @@
+sudo apt update
+sudo apt dist-upgrade -y
+mkdir ~/.ssh
+cdmod 777 ~/.ssh
+echo "HostKeyAlgorithms ssh-rsa">>~/.ssh/config
+cp -r /home/ubuntu/duckietown/ .
+echo "source ~/duckietown/environment.sh">>~/.bashrc
+cd ~/duckietown/catkin_ws/
+catkin_make
+echo "export DUCKIEFLEET_ROOT=~/duckiefleet">>~/.bashrc
+source ~/.bashrc
+cd
+git clone https://github.com/duckietown/duckiefleet.git
+echo "hostname: $1">>~/duckiefleet/robots/taiwan/$1.robot.yaml
+echo "username: $2">>~/duckiefleet/robots/taiwan/$1.robot.yaml
+echo "description: $3">>~/duckiefleet/robots/taiwan/$1.robot.yaml
+rosrun duckieteam create-machines
+echo "source ~/duckietown/set_ros_master.sh $1">>~/.bashrc
+source ~/.bashrc
