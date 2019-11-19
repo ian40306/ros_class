99bee04526a9fb42b37d218ac424c8d9aad11f72
ian40306 <43855922+ian40306@users.noreply.github.com>
2019-11-19 07:33:17 +0000

    Add files via upload

---
+++ step3.sh
@@ -0,0 +1,13 @@
+sudo chmod 777 ~/duckietown/catkin_ws ~/duckietown/catkin_ws/src
+cd ~/duckietown/catkin_ws/
+catkin_make
+echo "export DUCKIEFLEET_ROOT=~/duckiefleet">>~/.bashrc
+cd
+git clone https://github.com/duckietown/duckiefleet.git
+echo "hostname: $1">>~/duckiefleet/robots/taiwan/$1.robot.yaml
+echo "username: $2">>~/duckiefleet/robots/taiwan/$1.robot.yaml
+echo "description: $3">>~/duckiefleet/robots/taiwan/$1.robot.yaml
+sudo chmod 777 ~/duckietown/catkin_ws/src/00-infrastructure/duckietown
+rosrun duckieteam create-machines
+echo "source ~/duckietown/set_ros_master.sh $1">>~/.bashrc
+source ~/.bashrc
