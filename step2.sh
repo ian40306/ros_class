ac403e7bed1b61dca362310cf5ff91bab50ad310
ian40306 <43855922+ian40306@users.noreply.github.com>
2019-12-05 16:58:41 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -5,6 +5,7 @@ sudo apt dist-upgrade -y
 mkdir ~/.ssh
 chmod 777 ~/.ssh
 echo "HostKeyAlgorithms ssh-rsa">>~/.ssh/config
+echo "You are copy duckietown pkg now,please wait"
 sudo cp -r /home/ubuntu/duckietown/ .
 echo "source ~/duckietown/environment.sh">>~/.bashrc
 source ~/.bashrc
